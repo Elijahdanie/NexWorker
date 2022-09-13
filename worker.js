df6ebf056271fb1a6e6906a96135a34da5f73bc7
elijahdanie <elijahdanie@gmail.com>
2022-09-13 23:33:41 +0200

    update

--- worker.js
+++ worker.js
@@ -1,15 +1,15 @@
 const { start } = require('@nexrender/worker')
-
 const main = async () => {
     const serverHost = 'http://localhost:3000'
-    const serverSecret = 'no'
+    const serverSecret = process.argv[3]
     process.env['NEXRENDER_FFMPEG'] = '/usr/local/bin/ffmpeg'
     await start(serverHost, serverSecret, {
-        workpath: '/Users/applebusstop/Documents/Nexrender/.nexrender',
-        binary: '/Users/applebusstop/Applications/Adobe After Effects CC 2021/aerender',
-        skipCleanup: false,
+        workpath: 'C:/Users/Administrator/Documents/Nexrender/.nexrender',
+        binary: 'C:/Program Files/Adobe/Adobe After Effects 2022/Support Files/aerender',
+        skipCleanup: true,
         addLicense: false,
         debug: true,
+	stopOnError: false,
         actions: {
             "upload-media-server": (job, settings, {input, params}, type) => {
                 // Custom action code
